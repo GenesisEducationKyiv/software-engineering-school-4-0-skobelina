463718469c9008d22f2bf26b5dc89f2cd4f13c59
skobelina <ms.skobelina@gmail.com>
2024-06-17 02:52:52 +0300

    feat(scripts): add generate mock for mails

--- scripts/generate-mock.sh
+++ scripts/generate-mock.sh
@@ -3,8 +3,9 @@
 go get -d github.com/golang/mock/mockgen
 go install github.com/golang/mock/mockgen
 
-~/go/bin/mockgen -package=rates -self_package=github.com/skobelina/currency_converter/domains/rates -source=./domains/rates/service.go -mock_names Service=MockRateService -destination=./domains/rates/mock.go
-~/go/bin/mockgen -package=subscribers -self_package=github.com/skobelina/currency_converter/domains/subscribers -source=./domains/subscribers/service.go -mock_names Service=MockSubscriberService -destination=./domains/subscribers/mock.go
+~/go/bin/mockgen -package=rates -self_package=github.com/skobelina/currency_converter/domains/rates -source=./domains/rates/handler.go -mock_names Service=MockRateService -destination=./domains/rates/mock.go
+~/go/bin/mockgen -package=subscribers -self_package=github.com/skobelina/currency_converter/domains/subscribers -source=./domains/subscribers/handler.go -mock_names Service=MockSubscriberService -destination=./domains/subscribers/mock.go
+~/go/bin/mockgen -package=mails -self_package=github.com/skobelina/currency_converter/domains/mails -source=./domains/mails/service.go -mock_names Service=MockSubscriberService -destination=./domains/mails/mock.go
 
 go mod tidy
 
