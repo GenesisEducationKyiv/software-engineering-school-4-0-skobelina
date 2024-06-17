9a98571ce95d18e30d59819feb34be2185518881
skobelina <ms.skobelina@gmail.com>
2024-06-17 03:58:17 +0300

    feat(rates): add more errors

--- domains/rates/handler.go
+++ domains/rates/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 
 	utils "github.com/skobelina/currency_converter/utils/errors"
 	"github.com/skobelina/currency_converter/utils/rest"
@@ -36,10 +37,14 @@ func (h *handler) Register(r *mux.Router) {
 func (h *handler) get(w http.ResponseWriter, r *http.Request) error {
 	response, err := h.service.Get()
 	if err != nil {
-		if err.Error() == "bad request" {
+		if err == gorm.ErrRecordNotFound {
+			return utils.NewItemNotFoundError("rate not found")
+		} else if err.Error() == "bad request" {
 			return utils.NewBadRequestError("bad request")
-		} else {
+		} else if err.Error() == "internal server error" {
 			return utils.NewInternalServerError("internal server error")
+		} else {
+			return err
 		}
 	}
 	return serializer.SendJSON(w, http.StatusOK, response)
